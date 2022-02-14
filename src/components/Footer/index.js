809a7bba53452a4527183592a3e57c3b9160457b
Gregory Caloin <gregchapauli@gmail.com>
2022-02-14 18:15:35 +0100

    Mise en place de l'application

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>Projet réalisé par Greg - 2022</p>
+      <p>
+        Les icônes Wolverines, Iron-man, Spider-man et Batman proviennent du
+        site "iconFinder.com"
+      </p>
+    </div>
+  );
+};
+
+export default Footer;

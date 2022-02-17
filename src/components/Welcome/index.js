4482499135d1582d8a93175232fa0957a680402c
Gregory Caloin <gregchapauli@gmail.com>
2022-02-17 17:25:29 +0100

    Sécurisation de l'accès à la page Welcome aux utilisateurs authentifiés

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState, Fragment, useContext, useEffect } from "react";
 import Logout from "../Logout";
 import Quiz from "../Quiz";
+import { FirebaseContext } from "../Firebase";
+import { useNavigate } from "react-router-dom";
 
 const Welcome = () => {
-  return (
+  const firebase = useContext(FirebaseContext);
+
+  const [userSession, setUserSession] = useState(null);
+
+  const history = useNavigate();
+
+  //SECURISATION D'ACCES A LA PAGE WELCOME AUX UTILISATEURS AUTHENTIFIES
+  useEffect(() => {
+    let listener = firebase.auth.onAuthStateChanged((user) => {
+      user ? setUserSession(user) : history("/");
+    });
+    return () => {
+      listener();
+    };
+  }, [firebase.auth, history]);
+
+  return userSession === null ? (
+    <Fragment>
+      <div className="loader"></div>
+      <p className="loaderText">Loading...</p>
+    </Fragment>
+  ) : (
     <div className="quiz-bg">
       <div className="container">
         <Logout />

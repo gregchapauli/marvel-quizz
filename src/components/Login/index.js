7fd4a01aff0295f940175703d424bac82202301e
Gregory Caloin <gregchapauli@gmail.com>
2022-02-17 12:28:56 +0100

    Création de la page Login (formulaire, redirection, conditions de validité des champs)

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,9 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [btn, setBtn] = useState(false);
+
+  useEffect(() => {
+    if (password.length > 5 && email !== "") {
+      setBtn(true);
+    } else if (btn) {
+      setBtn(false);
+    }
+  }, [password, email, btn]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="signUpLoginBox">
-      <div className="slContainer">LOGIN</div>
+      <div className="slContainer">
+        {" "}
+        <div className="formBoxLeftLogin"></div>
+        <div className="formBoxRight">
+          <div className="formContent">
+            <h2>Connexion</h2>
+            <form onSubmit={handleSubmit}>
+              <div className="inputBox">
+                <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                  type="email"
+                  autoComplete="off"
+                  required
+                />
+                <label htmlFor="email">Email</label>
+              </div>
+              <div className="inputBox">
+                <input
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                  type="password"
+                  autoComplete="off"
+                  required
+                />
+                <label htmlFor="password">Mot de passe</label>
+              </div>
+
+              {btn ? (
+                <button>Connexion</button>
+              ) : (
+                <button disabled>Connexion</button>
+              )}
+            </form>
+            <div className="linkContainer">
+              <Link className="simpleLink" to="/Signup">
+                Nouveau sur Marvel Quizz? Inscrivez-vous maintenant.
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

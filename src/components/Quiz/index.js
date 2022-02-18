15009d16281544e9a067e263bdf5810e34a4eda1
Gregory Caloin <gregchapauli@gmail.com>
2022-02-18 21:35:43 +0100

    Récupération et affichage des questions et réponses dans le composant Quiz

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,24 +1,69 @@
 import React, { Component } from "react";
+import { QuizMarvel } from "../quizMarvel";
 import Levels from "../Levels";
 import ProgressBar from "../ProgressBar";
 
 // CE COMPOSANT EST UNE CLASS DANS UN BUT PEDAGOGIQUE
 class Quiz extends Component {
+  state = {
+    levelNames: ["debutant", "confirme", "expert"],
+    quizLevel: 0,
+    maxQuestions: 10,
+    storedQuestions: [],
+    question: null,
+    options: [],
+    idQuestion: 0,
+  };
+
+  loadQuestions = (level) => {
+    const fetchedArrayQuiz = QuizMarvel[0].quizz[level];
+    console.log(fetchedArrayQuiz);
+    if (fetchedArrayQuiz.length >= this.state.maxQuestions) {
+      // NOUVEAU TABLEAU SANS LES REPONSES POUR EVITER QU'ELLES SOIENT ACCESSIBLES DANS LA CONSOLE
+      const newArray = fetchedArrayQuiz.map(
+        ({ answer, ...keepRest }) => keepRest
+      );
+      this.setState({
+        storedQuestions: newArray,
+      });
+    } else {
+      console.log("Pas assez de questions!");
+    }
+  };
+
+  componentDidMount() {
+    this.loadQuestions(this.state.levelNames[this.state.quizLevel]);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.storedQuestions !== prevState.storedQuestions) {
+      this.setState({
+        question: this.state.storedQuestions[this.state.idQuestion].question,
+        options: this.state.storedQuestions[this.state.idQuestion].options,
+      });
+    }
+  }
+
   render() {
-    const { pseudo } = this.props.userData;
+    //const { pseudo } = this.props.userData;
+
+    const displayOptions = this.state.options.map((option, index) => {
+      return (
+        <p key={index} className="answerOptions">
+          {option}
+        </p>
+      );
+    });
 
     return (
       <div>
-        <h2>Pseudo: {pseudo}</h2>
+        {/*<h2>Pseudo: {pseudo}</h2>*/}
 
         <Levels />
         <ProgressBar />
 
-        <h2>Notre question quiz</h2>
-        <p className="answerOptions">Question 1</p>
-        <p className="answerOptions">Question 2</p>
-        <p className="answerOptions">Question 3</p>
-        <p className="answerOptions">Question 4</p>
+        <h2>{this.state.question}</h2>
+        {displayOptions}
         <button className="btnSubmit">Suivant</button>
       </div>
     );

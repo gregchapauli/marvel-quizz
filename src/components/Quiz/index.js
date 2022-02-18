5d796bc7aa05137d8839f0e1e956f92807e681d0
Gregory Caloin <gregchapauli@gmail.com>
2022-02-18 18:09:36 +0100

    Mise en place du Quiz avec les composants Levels et ProgressBar

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
+import Levels from "../Levels";
+import ProgressBar from "../ProgressBar";
 
+// CE COMPOSANT EST UNE CLASS DANS UN BUT PEDAGOGIQUE
 class Quiz extends Component {
   render() {
     const { pseudo } = this.props.userData;
 
     return (
       <div>
-        <h2>Pseudo: {{ pseudo }}</h2>
+        <h2>Pseudo: {pseudo}</h2>
+
+        <Levels />
+        <ProgressBar />
+
+        <h2>Notre question quiz</h2>
+        <p className="answerOptions">Question 1</p>
+        <p className="answerOptions">Question 2</p>
+        <p className="answerOptions">Question 3</p>
+        <p className="answerOptions">Question 4</p>
+        <button className="btnSubmit">Suivant</button>
       </div>
     );
   }

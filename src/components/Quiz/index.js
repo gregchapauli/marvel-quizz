1d6acd0f2372b350a9117c0f5e139ecca77a0012
Gregory Caloin <gregchapauli@gmail.com>
2022-02-19 11:31:45 +0100

    Validation de la réponse et gestion du score

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -15,12 +15,19 @@ class Quiz extends Component {
     idQuestion: 0,
     btnDisable: true,
     userAnswer: null,
+    score: 0,
   };
+
+  //UTILISATION DU REF DANS UN BUT PEDAGOGIQUE POUR STOCKER DE LA DATA
+  storedDataRef = React.createRef();
 
   loadQuestions = (level) => {
     const fetchedArrayQuiz = QuizMarvel[0].quizz[level];
     console.log(fetchedArrayQuiz);
     if (fetchedArrayQuiz.length >= this.state.maxQuestions) {
+      //STOCKAGE DU TABLEAU DANS LE REF
+      this.storedDataRef.current = fetchedArrayQuiz;
+
       // NOUVEAU TABLEAU SANS LES REPONSES POUR EVITER QU'ELLES SOIENT ACCESSIBLES DANS LA CONSOLE
       const newArray = fetchedArrayQuiz.map(
         ({ answer, ...keepRest }) => keepRest
@@ -37,11 +44,37 @@ class Quiz extends Component {
     this.loadQuestions(this.state.levelNames[this.state.quizLevel]);
   }
 
+  nextQuestion = () => {
+    if (this.state.idQuestion === this.state.maxQuestions - 1) {
+      //END
+    } else {
+      this.setState((prevState) => ({
+        idQuestion: prevState.idQuestion + 1,
+      }));
+    }
+    const goodAnswer = this.storedDataRef.current[this.state.idQuestion].answer;
+    if (this.state.userAnswer === goodAnswer) {
+      this.setState((prevState) => ({
+        score: this.prevState.score + 1,
+      }));
+    }
+  };
+
   componentDidUpdate(prevProps, prevState) {
     if (this.state.storedQuestions !== prevState.storedQuestions) {
       this.setState({
         question: this.state.storedQuestions[this.state.idQuestion].question,
         options: this.state.storedQuestions[this.state.idQuestion].options,
+      });
+    }
+
+    // MISE A JOUR DES QUESTIONS QUAND LE STATE A CHANGE
+    if (this.state.idQuestion !== prevState.idQuestion) {
+      this.setState({
+        question: this.state.storedQuestions[this.state.idQuestion].question,
+        options: this.state.storedQuestions[this.state.idQuestion].options,
+        userAnswer: null,
+        btnDisable: true,
       });
     }
   }
@@ -82,7 +115,11 @@ class Quiz extends Component {
 
         <h2>{this.state.question}</h2>
         {displayOptions}
-        <button disabled={this.state.btnDisable} className="btnSubmit">
+        <button
+          disabled={this.state.btnDisable}
+          className="btnSubmit"
+          onClick={this.nextQuestion}
+        >
           Suivant
         </button>
       </div>

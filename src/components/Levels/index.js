f4a9ef4bab490c2ea90f65aade05bc3754d12003
Gregory Caloin <gregchapauli@gmail.com>
2022-02-23 17:18:03 +0100

    Mise en forme du Stepper

--- src/components/Levels/index.js
+++ src/components/Levels/index.js
@@ -12,8 +12,21 @@ const Levels = ({ levelNames, quizLevel }) => {
   }, [levelNames]);
 
   return (
-    <div className="levelsContainer">
-      <Stepper steps={levels} activeStep={quizLevel} />
+    <div className="levelsContainer" style={{ background: "transparent" }}>
+      <Stepper
+        steps={levels}
+        activeStep={quizLevel}
+        circleTop={0}
+        activeTitleColor={"#d31017"}
+        activeColor={"#d31017"}
+        completeTitleColor={"#E0E0E0"}
+        defaultTitleColor={"#E0E0E0"}
+        completeColor={"#E0E0E0"}
+        completeBarColor={"#E0E0E0"}
+        barStyle={"dashed"}
+        size={45}
+        circleFontSize={20}
+      />
     </div>
   );
 };

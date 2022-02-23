7f6c1a5c1cbf36a7f1a5480119cb5c465aa6c858
Gregory Caloin <gregchapauli@gmail.com>
2022-02-23 16:49:15 +0100

    Création du composant "Levels" et importation du composant "Stepper"

--- src/components/Levels/index.js
+++ src/components/Levels/index.js
@@ -1,9 +1,20 @@
 import React from "react";
+import Stepper from "react-stepper-horizontal";
 
 const Levels = () => {
   return (
     <div className="levelsContainer">
-      <h2 className="headingLevels">Débutant</h2>
+      <div>
+        <Stepper
+          steps={[
+            { title: "Step One" },
+            { title: "Step Two" },
+            { title: "Step Three" },
+            { title: "Step Four" },
+          ]}
+          activeStep={1}
+        />
+      </div>
     </div>
   );
 };

281c642fdb4181110a896b697d8d4461131749aa
Gregory Caloin <gregchapauli@gmail.com>
2022-03-04 18:13:37 +0100

    Mise en place des variables d'environnement

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -3,12 +3,12 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDOw_cs0KHL3Mwq2e_o5zbtVwe01NNOIew",
-  authDomain: "marvel-quizz-10c02.firebaseapp.com",
-  projectId: "marvel-quizz-10c02",
-  storageBucket: "marvel-quizz-10c02.appspot.com",
-  messagingSenderId: "36627023156",
-  appId: "1:36627023156:web:3d9112cce363246684d161",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 class Firebase {
